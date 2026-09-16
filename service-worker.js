cee0bd888fd92e60fc7503bfb559a8a8f67823c8
hafezsajidurrahman-tech <hafezsajidurrahman@gmail.com>
2026-09-16 15:27:23 +0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,28 @@
-const CACHE_NAME = "arbin-sajid-v1";
+const CACHE_NAME = "arbin-sajid-v2";
 
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
   "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
+  "./file_0000000087a882309e4754a79eac4fbc.png"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
   self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
+    caches.keys().then(keys =>
+      Promise.all(
         keys
           .filter(key => key !== CACHE_NAME)
           .map(key => caches.delete(key))
-      );
-    })
+      )
+    )
   );
   self.clients.claim();
 });
